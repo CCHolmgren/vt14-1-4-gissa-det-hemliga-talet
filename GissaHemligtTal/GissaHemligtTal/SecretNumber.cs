d9779481e5e8338476218c74b503e791bb613c7d
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-02-04 16:18:13 +0100

    Initial

---
+++ GissaHemligtTal/GissaHemligtTal/SecretNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GissaHemligtTal
+{
+    public class SecretNumber
+    {
+        const int MaxNumberOfGuesses;
+        int _number;
+        List<int> _previousGuesses;
+
+    }
+}
